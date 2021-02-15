921c687aaa7ec795ce02345d47a4d0a7d5821788
Benjamin <benjaminevans316@gmail.com>
2021-02-15 13:49:17 +0000

    Reworked test functions

--- TestingRacing.py
+++ TestingRacing.py
@@ -1,18 +1,16 @@
-from Rewards import CrossTrackHeadingReward, ModHeadingReward, ModStdTimeReward, OnlineSteering, StdNavReward
 import numpy as np
 import csv, yaml
+from Rewards import CthReward, TimeReward, SteerReward
 
 
 import LibFunctions as lib
+from LibFunctions import load_config
 
 # from AgentOptimal import OptimalAgent
+from AgentOptimal import TunerCar
 from AgentMod import ModVehicleTest, ModVehicleTrain
 from RefGen import GenTest, GenVehicle
 
-names = ['columbia', 'levine_blocked', 'mtl', 'porto', 'torino', 'race_track']
-name = names[5]
-myMap = 'TrackMap1000'
-forest_name = 'forest'
 
 config_sf = "small_forest"
 config_std = "std_config"
@@ -20,98 +18,113 @@ config_std = "std_config"
 
 from Testing import TestVehicles, TrainVehicle
 
+config_sf = "small_forest"
+config_std = "std_config"
+config_med = "med_forest"
 
 
 """ Training sets"""
-def train_gen_std():
+def train_gen_time():
     load = False
 
-    agent_name = "GenStd_0_02_0"
-    config = load_config("std_config")
+    agent_name = "GenTime_test"
+    config = load_config(config_med)
     vehicle = GenVehicle(config, agent_name, load)
-    reward = StdNavReward(config, 0, 0.2, 0)
+    reward = TimeReward(config, 0.06)
 
-    TrainVehicle(config, agent_name, vehicle, reward)
+    TrainVehicle(config, agent_name, vehicle, reward, 4000)
 
 def train_gen_cth():
     load = False
 
-    agent_name = "GenCth_1_1_1"
-    config = load_config("std_config")
+    agent_name = "GenCth_test"
+    config = load_config(config_med)
     vehicle = GenVehicle(config, agent_name, load)
-    reward = CrossTrackHeadingReward(config, 1, 1, 1)
+    reward = CthReward(config, 0.4, 0.04)
 
-    TrainVehicle(config, agent_name, vehicle, reward)
+    TrainVehicle(config, agent_name, vehicle, reward, 4000)
 
 def train_gen_steer():
     load = False
 
-    agent_name = "GenSteer_02_02"
-    config = load_config("std_config")
+    agent_name = "GenSteer_test"
+    config = load_config(config_med)
     vehicle = GenVehicle(config, agent_name, load)
-    reward = OnlineSteering(config, 0.2, 0.2)
+    reward = SteerReward(config, 0.1, 0.1)
 
-    TrainVehicle(config, agent_name, vehicle, reward)
+    TrainVehicle(config, agent_name, vehicle, reward, 4000)
 
 """Mod training"""
-def train_mod_std():
+def train_mod_steer():
     load = False
 
-    agent_name = "ModStd_04_02_0"
-    config = load_config("std_config")
+    agent_name = "ModSteer_test"
+    config = load_config(config_med)
     vehicle = ModVehicleTrain(config, agent_name, load)
-    reward = ModStdTimeReward(config, 0.4, 0.2, 0)
+    reward = SteerReward(config, 0.1, 0.1)
 
-    TrainVehicle(config, agent_name, vehicle, reward)
+    TrainVehicle(config, agent_name, vehicle, reward, 4000)
 
 def train_mod_time():
     load = False
 
-    agent_name = "ModTime_01_01_1"
-    config = load_config("std_config")
+    agent_name = "ModTime_test"
+    config = load_config(config_med)
     vehicle = ModVehicleTrain(config, agent_name, load)
-    reward = ModStdTimeReward(config, 0.1, 0.1, 1)
+    reward = TimeReward(config, 0.06)
 
-    TrainVehicle(config, agent_name, vehicle, reward)
+    TrainVehicle(config, agent_name, vehicle, reward, 4000)
 
 def train_mod_cth():
     load = False
 
-    agent_name = "ModCth_1_1_1"
-    config = load_config("std_config")
+    agent_name = "ModCth_test"
+    config = load_config(config_med)
     vehicle = ModVehicleTrain(config, agent_name, load)
-    reward = ModHeadingReward(config, 1, 1, 1)
+    reward = CthReward(config, 0.4, 0.04)
 
-    TrainVehicle(config, agent_name, vehicle, reward)
+    TrainVehicle(config, agent_name, vehicle, reward, 4000)
 
 
+"""Tests """
 def test_compare():
-    config = load_config("std_config")
-    test = TestVehicles(config, 'RaceComparison_t')
+    config = load_config(config_med)
+    # config = load_config(config_std)
+    # test = TestVehicles(config, "test_compare_mod")
+    # test = TestVehicles(config, "test_compare_gen")
+    test = TestVehicles(config, "test_compare")
 
-    agent_name = "GenStd_0_02_0"
-    vehicle = GenTest(config, agent_name)
-    # test.add_vehicle(vehicle)
-
-    agent_name = "GenCth_1_1_1"
-    # vehicle = GenTest(config, agent_name)
-    # test.add_vehicle(vehicle)
-
-    agent_name = "GenSteer_02_02"
-    vehicle = GenTest(config, agent_name)
-    # test.add_vehicle(vehicle)
-
-    agent_name = "ModStd_04_02_0"
+    # mod
+    agent_name = "ModTime_test"
     vehicle = ModVehicleTest(config, agent_name)
     test.add_vehicle(vehicle)
 
-    agent_name = "ModTime_01_01_1"
-    # vehicle = ModVehicleTest(config, agent_name)
-    # test.add_vehicle(vehicle)
+    agent_name = "ModCth_test"
+    vehicle = ModVehicleTest(config, agent_name)
+    test.add_vehicle(vehicle)
 
-    agent_name = "ModCth_1_1_1"
-    # vehicle = GenTest(config, agent_name)
-    # test.add_vehicle(vehicle)
+    agent_name = "ModSteer_test"
+    vehicle = ModVehicleTest(config, agent_name)
+    test.add_vehicle(vehicle)
+
+    # gen
+    agent_name = "GenTime_test"
+    vehicle = GenTest(config, agent_name)
+    test.add_vehicle(vehicle)
+
+    agent_name = "GenCth_test"
+    vehicle = GenTest(config, agent_name)
+    test.add_vehicle(vehicle)
+
+    agent_name = "GenSteer_test"
+    vehicle = GenTest(config, agent_name)
+    test.add_vehicle(vehicle)
+
+    # PP
+    vehicle = TunerCar(config)
+    test.add_vehicle(vehicle)
+
+    test.run_eval(100, True)
 
 
 
@@ -159,6 +172,8 @@ def test_compare_std():
 
 
     test.run_eval(10, True)
+
+
 
 
 def train():
